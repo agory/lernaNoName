9fd84d4bbb4d883b1c15a4059732d9a205d2c8f5
agory <gory.alexandre02@gmail.com>
2018-06-12 10:57:54 +0200

    first test with lerna publish

---
+++ packages/server/pages/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import WebConnector from 'rn-noname-connector/build/WebConnector';
+import Main from '../src/web/layout/Main';
+import TreeNavigator from '../src/web/widget/TreeNavigator';
+
+
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tree: null,
+    };
+    this.updateTreeView = this.updateTreeView.bind(this);
+  }
+
+  componentDidMount() {
+    const connector = new WebConnector({});
+    this.subscription = connector.getTreeView().subscribe(this.updateTreeView);
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+  updateTreeView(tree) {
+    this.setState({ tree });
+  }
+
+  render() {
+    const { tree } = this.state;
+    return (
+      <Main title="TreeView">
+        {tree ? <TreeNavigator tree={tree.root} /> : <p>No devices connected</p>}
+      </Main>
+    );
+  }
+}
+export default index;
